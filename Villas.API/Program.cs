bc372fd3a333b25da4c148e53e175241265d53c7
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-10 22:56:24 +0530

    added scalar (API Testing)

--- Villas.API/Program.cs
+++ Villas.API/Program.cs
@@ -1,4 +1,6 @@
 
+using Scalar.AspNetCore;
+
 namespace Villas.API
 {
     public class Program
@@ -7,18 +9,16 @@ namespace Villas.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddControllers();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
