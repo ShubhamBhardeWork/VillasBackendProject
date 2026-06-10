d5a4732a13ddda04db1eb537e6655b1db73d94fb
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-10 23:34:28 +0530

    feat: added mock data for villas GET endpoint

---
+++ Villas.API/Controllers/VillasController.cs
@@ -0,0 +1,22 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+
+    namespace Villas.API.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class VillasController : ControllerBase
+        {
+            private static readonly List<string> _villas = new List<string>
+            {
+                "Royal Sunshine Villa",
+                "Blue Ridge Villa",
+                "Next To Way Villa"
+            };
+
+            [HttpGet]
+            public IActionResult GetVillas()
+            {
+                return Ok(_villas);
+            }
+        }
+    }
