14c010e853d08aba93e0fbefd4b530bcd5241e7c
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-11 20:39:33 +0530

    feat: create AppDbContext

---
+++ Villas.API/Data/VillaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Villas.API.Models.Domain;
+
+namespace Villas.API.Data
+{
+    public class VillaDbContext : DbContext
+    {
+        public VillaDbContext(DbContextOptions<VillaDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+    }
+}
