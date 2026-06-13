007b0ba0d8cb95772c21a35a16b66d5083ac4e0a
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-13 22:45:12 +0530

    feat: implement villas GET endpoint

--- Villas.API/Controllers/VillasController.cs
+++ Villas.API/Controllers/VillasController.cs
@@ -1,29 +1,55 @@
-﻿    using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Villas.API.Data;
+using Villas.API.DTOs;
 
-    namespace Villas.API.Controllers
+namespace Villas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillasController : ControllerBase
     {
-        [Route("api/[controller]")]
-        [ApiController]
-        public class VillasController : ControllerBase
+        private readonly VillaDbContext _context;
+        public VillasController(VillaDbContext context)
         {
-            private static readonly List<string> _villas = new List<string>
-            {
-                "Royal Sunshine Villa",
-                "Blue Ridge Villa",
-                "Next To Way Villa"
-            };
+            _context = context;
+        }
 
-            [HttpGet]
-            public IActionResult GetVillas()
+
+        [HttpGet]
+        public async Task<IActionResult> GetVillas()
+        {
+            try
             {
-                return Ok(_villas);
+                var villas = await _context.Villas.AsNoTracking().ToListAsync();
+                var villasResponse = villas.Select(v => new VillaResponse
+                {
+                    Id = v.Id,
+                    Name = v.Name,
+                    Details = v.Details,
+                    Rate = v.Rate,
+                    Sqft = v.Sqft,
+                    Occupancy = v.Occupancy,
+                    ImageUrl = v.ImageUrl,
+                    CreatedAt = v.CreatedAt,
+                    LastUpdatedAt = v.LastUpdatedAt
+                });
+
+                //throw new Exception("Test exception for error handling validation.");
+
+                return Ok(villasResponse);
             }
-
-            [HttpGet]
-            [Route("{id}")]
-            public IActionResult GetVillaById([FromRoute] int id)
+            catch (Exception ex)
             {
-                return Ok($"Villa Id:- {id}");
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new
+                    {   
+                        Status = false,
+                        Message = "Something went wrong.",
+                    }
+                );
             }
         }
     }
+}
