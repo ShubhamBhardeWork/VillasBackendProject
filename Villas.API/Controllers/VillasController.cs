a1d056160b73359659a876357fba935a2276d8a6
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-13 23:05:22 +0530

    feat: implement get villa by id endpoint

--- Villas.API/Controllers/VillasController.cs
+++ Villas.API/Controllers/VillasController.cs
@@ -51,5 +51,53 @@ namespace Villas.API.Controllers
                 );
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetVillaById([FromRoute] int id)
+        {
+            try
+            {
+                if(id < 1)
+                {
+                    return BadRequest("Please enter valid id which is greater than 0.");
+                }
+
+                var existingVilla = await _context.Villas.AsNoTracking().FirstOrDefaultAsync(v => v.Id == id);
+
+                if(existingVilla is null)
+                {
+                    return NotFound($"Villa with Id {id} not found.");
+                }
+
+                var villaResponse = new VillaResponse
+                {
+                    Id = existingVilla.Id,
+                    Name = existingVilla.Name,
+                    Details = existingVilla.Details,
+                    Rate = existingVilla.Rate,
+                    Sqft = existingVilla.Sqft,
+                    Occupancy = existingVilla.Occupancy,
+                    ImageUrl = existingVilla.ImageUrl,
+                    CreatedAt = existingVilla.CreatedAt,
+                    LastUpdatedAt = existingVilla.LastUpdatedAt
+                };
+
+                return Ok(villaResponse);
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError, 
+                    new
+                    {
+                        Success = false,
+                        Message = "Something went wrong."
+                    }
+                );
+
+            }
+        }
     }
 }
