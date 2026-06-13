46ea7bc7a88f4fc436146002f413b688c09d1aaa
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-14 04:41:25 +0530

    feat: validate update villa request using data annotations

--- Villas.API/DTOs/UpdateVillaRequest.cs
+++ Villas.API/DTOs/UpdateVillaRequest.cs
@@ -1,12 +1,28 @@
-﻿namespace Villas.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villas.API.DTOs
 {
     public class UpdateVillaRequest
     {
+        [Required]
+        [MaxLength(50)]
         public required string Name { get; set; }
+
+        [MaxLength(200)]
         public string? Details { get; set; }
+
+        [Required]
+        [Range(1, double.MaxValue)]
         public decimal Rate { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Sqft { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Occupancy { get; set; }
+
         public string? ImageUrl { get; set; }
     }
 }
