d3dabf97b9e2a33db00a101cc5f1992449cbc194
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-20 23:59:58 +0530

    test: add k6 stress test for GET villas endpoint

---
+++ Villas.API/PerformanceTests/get-villas-stress.js
@@ -0,0 +1,20 @@
+﻿import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '20s', target: 10 },
+    { duration: '20s', target: 25 },
+    { duration: '20s', target: 50 },
+    { duration: '20s', target: 100 },
+    { duration: '20s', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get('https://localhost:8000/api/villas');
+
+check(res, {
+    'GET /api/villas status is 200': (r) => r.status === 200,
+  });
+}
