7bca2e1aee896edb0ecf09fcfcf96bf0a264dc5e
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-28 22:53:53 +0530

    feat: add User domain model

---
+++ Villas.API/Models/Domain/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Villas.API.Models.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+    }
+}
