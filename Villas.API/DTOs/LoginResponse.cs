d5f71e114dc5a52237a08d7ec8bebcc63f30ab29
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-28 23:39:13 +0530

    feat: add authentication DTOs

---
+++ Villas.API/DTOs/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Villas.API.DTOs
+{
+    public class LoginResponse
+    {
+        public string? AccessToken { get; set; } 
+        public UserResponse? User { get; set; }
+    }
+}
---
+++ Villas.API/DTOs/RegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Villas.API.DTOs
+{
+    public class RegisterRequest
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
