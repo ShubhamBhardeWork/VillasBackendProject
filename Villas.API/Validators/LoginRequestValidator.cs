234278cbc2c3819ad6797375a7def5fcb72f3e05
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-28 23:39:53 +0530

    feat: add FluentValidation validators for authentication

---
+++ Villas.API/Validators/LoginRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Villas.API.DTOs;
+
+
+namespace Villas.API.Validators;
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+
+}
---
+++ Villas.API/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Villas.API.DTOs;
+
+namespace Villas.API.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(30);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8)
+                .MaximumLength(100);
+        }
+    }
+}
